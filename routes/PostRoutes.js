2d4bd612af641d0f85758ccd1609a069a592aa46
Sauvik Nath <sauviknath10@gmail.com>
2023-03-25 02:36:33 +0530

    upvote and downvote controller

--- routes/PostRoutes.js
+++ routes/PostRoutes.js
@@ -2,31 +2,39 @@ const express = require("express");
 const PostRouter = new express.Router();
 const auth = require("../middleware/auth");
 const {
-    createPost,
-    getPosts,
-    getPostsByKeywordAndCategory,
-    getPostById,
-    updatePost,
-    deletePost,
-  } = require("../controller/PostController");
+  createPost,
+  getPosts,
+  getPostsByKeywordAndCategory,
+  getPostById,
+  upvotePost,
+  downvotePost,
+  updatePost,
+  deletePost,
+} = require("../controller/PostController");
 
-  // create new post
-  PostRouter.post("/posts", createPost);
+// create new post
+PostRouter.post("/posts", createPost);
 
-  // get all posts
-  PostRouter.get("/posts", getPosts);
+// get all posts
+PostRouter.get("/posts", getPosts);
 
-  // make GET requests to /posts/search?text=keyword&category=categoryname
-  PostRouter.get('/posts/search', getPostsByKeywordAndCategory);
+// make GET requests to /posts/search?text=keyword&category=categoryname
+PostRouter.get('/posts/search', getPostsByKeywordAndCategory);
 
-  // get post with a postId
-  PostRouter.get("/posts/:postId", getPostById);
+// get post with a postId
+PostRouter.get("/posts/:postId", getPostById);
 
-  // update a post
-  PostRouter.patch("/posts/:postId", auth, updatePost);
+// upvote a post
+PostRouter.patch("/posts/:postId/upvote", auth, upvotePost);
 
-  // delete a post
-  PostRouter.delete("/posts/:postId", auth, deletePost);
-  
+// downvote a post
+PostRouter.patch("/posts/:postId/downvote", auth, downvotePost);
 
-  module.exports = {PostRoutes:PostRouter};
+// update a post
+PostRouter.patch("/posts/:postId", auth, updatePost);
+
+// delete a post
+PostRouter.delete("/posts/:postId", auth, deletePost);
+
+
+module.exports = { PostRoutes: PostRouter };
